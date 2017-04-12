6ef215e54539de1a04ba527022d4145fa2570e00
Facundo Nicolás Orlando <FacundoNicolas@Mac-mini-de-Agus.local>
2017-04-12 18:03:42 -0300

    Se cambio de color el menu, se agrego un borde parecido a una sombra, se agrego el boton e input agregar contenido pero falta mejorar

--- com.js
+++ com.js
@@ -7,15 +7,15 @@ $(document).ready(function() {
     var x               = $("#contenedor div").length + 1; //var x = número de campos existentes en el contenedor
     var conteo          = x-1; //para el seguimiento de los campos
 
-    $(AddButton).on('click', function(e) {
+    $(AddButton).on('click', function(e) { // Al clickear el AddButton
 
         if(x <= MaxInputs){ //maximos de secciones impresas
 
-              var val   = $("#input").val(); // Toma el valor del Input
+              var val   = $("#inputSec").val(); // Toma el valor del Input
 
           if(val==""){ // si el Input no tiene nada
               alert("Por favor ponle nombre a la sección que vas a crear");
-              $('#input').focus();
+              $('#inputSec').focus();
               return false;
           }else{
               if( $('#'+val ).length > 0){  // Si el id #Con el valor que tiene el input en ese momento existe
@@ -34,7 +34,13 @@ $(document).ready(function() {
           x++; //Incrementa el numero
         };
     });
+      var contenido  = $("#inputCont").val();
 
+    $("#agregarContenido").on('click', function(){
+      $("div ul").append("<li>"+ contenido +"</li>");
+    });
+
+//  ELIMINAR SECCIÓN ******************************************
     $("#contenedor").on("click",".eliminar", function(e){ //Click en eliminar campo
         if( x > 1 ) {
             $(this).parent('div').remove(); //Eliminar el campo
@@ -43,9 +49,13 @@ $(document).ready(function() {
         return false;
     });
 
+
+
+//MUESTRA EL MENU PARA CREAR SECCIÓN Y AGREGAR CAMPOS *********
     $('#menuToggle').on('click', function(){
-      $('.cuerpo').toggleClass('abrir');
-      $('.cuerpo #modal').toggleClass('modal');
+      $('.cuerpo').toggleClass('abrir'); //Agrega o saca la clase Abrir dependiendo si la tiene o no hace lo contrario
+      $('.cuerpo #modal').toggleClass('modal'); //Agrega o saca la clase modal dependiendo si la tiene o no hace lo contrario
+      $('.cuerpo').toggleClass('bordeCuerpo'); //Agrega o saca la clase bordeCuerpo que hace como sombra dependiendo si la tiene o no hace lo contrario
     });
 
 });
